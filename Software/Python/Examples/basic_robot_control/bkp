aa208346588e5b543cb4a60be5923a016f9a6661
root <root@raspberrypi.(none)>
2014-06-13 10:22:31 +0000

    Documentation for the examples added

---
+++ Software/Python/Examples/basic_robot_control/bkp
@@ -0,0 +1,27 @@
+from gopigo import *	#Has the basic functions for controlling the GoPiGo Robot
+import sys	#Used for closing the running program
+print "This is a basic example for the GoPiGo Robot control"
+print "Press:\n\tw: Move GoPiGo Robot forward\n\ta: Turn GoPiGo Robot left\n\td: Turn GoPiGo Robot right\n\ts: Move GoPiGo Robot backward\n\tt: Increase speed\n\tg: Decrease speed\n\tx: Stop GoPiGo Robot\n\tz: Exit\n"
+while True:
+	print "Enter the Command:",
+	a=raw_input()	# Fetch the input from the terminal
+	if a=='w':
+		fwd()	# Move forward
+	elif a=='a':
+		left()	# Turn left
+	elif a=='d':
+		right()	# Turn Right
+	elif a=='s':
+		bwd()	# Move back
+	elif a=='x':
+		stop()	# Stop
+	elif a=='t':
+		increase_speed()	# Increase speed
+	elif a=='g':
+		decrease_speed()	# Decrease speed
+	elif a=='z':
+		print "Exiting"		# Exit
+		sys.exit()
+	else:
+		print "Wrong Command, Please Enter Again"
+	time.sleep(.1)
--- Software/Python/Examples/mouse_control/Mouse_Control_example.py
+++ Software/Python/Examples/mouse_control/Mouse_Control_example.py
@@ -1,9 +1,20 @@
+# Karan
+# Initial Date: June 13, 2014
+#
+# These files have been made available online through a Creative Commons Attribution-ShareAlike 3.0  license.
+# (http://creativecommons.org/licenses/by-sa/3.0/)
+#
+# http://www.dexterindustries.com/
+#
+# This example is for controlling the GoPiGo robot from a mouse connected to the Raspberry Pi
+
 import struct
 import sys
 from gopigo import *
+
 file = open( "/dev/input/mice", "rb" );
 
-debug = 0
+debug = 0	#Print raw values when debugging
 def getMouseEvent():
 	buf = file.read(3)
 	button = ord( buf[0] )
@@ -17,22 +28,23 @@ def getMouseEvent():
 	
 flag=0
 print "Press Enter to start"
-a=raw_input()
+a=raw_input()	#Wait for an input to start
 fwd()
 while( 1 ):
-	[l,m,r,x,y]=getMouseEvent()
+	[l,m,r,x,y]=getMouseEvent()	#Get the inputs from the mouse
 	if debug:
 		print l,m,r,x,y
-	if flag==1:
+		
+	if flag==1: #If left or right mouse not pressed, move forward
 		fwd()
 		flag=0
-	if l:
+	if l:		#If left mouse buton pressed, turn left
 		left()
 		flag=1
-	if m:
+	if m:		#If middle mouse button pressed, stop
 		stop()
-	if r:
+	if r:		#If right mouse button presses, turn right
 		right()
 		flag=1
-	if l and r:
+	if l and r:	#If both the left and right mouse buttons pressed, go back
 		bwd()
