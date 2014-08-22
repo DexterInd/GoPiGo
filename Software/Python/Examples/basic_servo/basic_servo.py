4138af23d52d47895e4e31def80eb9b5f9bb406c
Dexter Industries <dxtrindustries@gmail.com>
2014-08-22 15:50:36 +0530

    Basic servo touchup.
    Touched up the basic servo example

--- Software/Python/Examples/basic_servo/basic_servo.py
+++ Software/Python/Examples/basic_servo/basic_servo.py
@@ -1,6 +1,11 @@
 #!/usr/bin/python
 ########################################################################                                                                  
-# This example is for controlling the Servo on the GoPiGo robot from the keyboard                            
+# This example demonstrates controlling the Servo on the GoPiGo robot.
+# In this example, we control the servo with a keyboard.  When you run
+# this example from the command line, you'll be prompted for input
+# Press a key (a, d, or s) to move the servo.  The data is collected, 
+# and sent to the GoPiGo.
+#                            
 # http://www.dexterindustries.com/GoPiGo/                                                                
 # History
 # ------------------------------------------------
@@ -22,13 +27,14 @@ print "Press ENTER to send the commands"
 
 while True:
 	#Get the input from the user and change the servo angles
-	inp=raw_input()
+	inp=raw_input()				# Get keyboard input.
+	# Now decide what to do with that keyboard input.  
 	if inp=='a':
-		servo_pos=servo_pos+10
+		servo_pos=servo_pos+10	# If input is 'a' move the servo forward 10 degrees.
 	elif inp=='d':
-		servo_pos=servo_pos-10
+		servo_pos=servo_pos-10	# If the input is 'd' move the servo backward by 10 degrees.
 	elif inp=='s':
-		servo_pos=90
+		servo_pos=90			
 		
 	#Get the servo angles back to the normal 0 to 180 degree range
 	if servo_pos>180:
@@ -36,6 +42,6 @@ while True:
 	if servo_pos<0:
 		servo_pos=0
 		
-	servo(servo_pos)#Move the servo
-	time.sleep(.1)
+	servo(servo_pos)		# This function updates the servo with the latest positon.  Move the servo.
+	time.sleep(.1)			# Take a break in between operations.  
 	
