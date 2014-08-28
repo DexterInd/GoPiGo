7617f0ba02e4b91359ab1e35db1c582e5d457fe1
Dexter Industries <dxtrindustries@gmail.com>
2014-08-28 09:41:44 +0530

    Renamed directory.
    Renamed directory to "robot" not bot.

---
+++ Software/Python/Examples/Compass Robot/compass_bot.py
@@ -0,0 +1,142 @@
+#!/usr/bin/python
+##################################################################################################################
+<<<<<<< HEAD
+# This is a project example for using the GoPiGo with the compass as a turtle robot
+=======
+# This is a project example for uning the GoPiGo with the compass as a turtle robot
+>>>>>>> parent of 1924e7e... File name changes.
+#                             
+# http://www.dexterindustries.com/GrovePi/        
+# Compass module: http://www.seeedstudio.com/depot/Grove-3Axis-Digital-Compass-p-759.html
+#                                                       
+# History
+# ------------------------------------------------
+# Author     Date      		Comments
+# Karan      22 July 14  	Initial Authoring
+# 			                                                             
+# These files have been made available online through a Creative Commons Attribution-ShareAlike 3.0  license.
+# (http://creativecommons.org/licenses/by-sa/3.0/)           
+#
+# Refer to the datasheet to add additional functionality https://www.seeedstudio.com/wiki/images/4/42/HMC5883.pdf
+#
+# Command:
+#	f dist 	:	move forward by "dist" (distance = dist/4 encoder counts. 1 rotation=18 encoder counts) e.g.: f 100
+#	l 45	:	rotate left by 45 degrees
+#	r 45	:	rotate right by 45 degrees
+#
+#	NOTE	:	There might be a bug with the Grove Compass which causes it to rotate by more than what you want.
+<<<<<<< HEAD
+#					Try the compass test to see that you get proper reading when you rotate the compass 360 degrees in
+=======
+#					Try the compass test to see that you get proper reading when you rotate the compas 360 degrees in
+>>>>>>> parent of 1924e7e... File name changes.
+#				90 degree increments.
+####################################################################################################################
+
+from grove_compass_lib import *
+from gopigo import *
+import turtle
+
+en_turtle=1		#Enable turtle graphics
+debug=0			#Disable debug mode
+
+if en_turtle:
+	turtle.Turtle()
+	
+c=compass()
+divider=4		
+
+set_speed(110)
+
+while True:
+	print "CMD:",			#Wait for a command
+	cmd=raw_input()
+	print cmd
+	try:
+		if cmd[0]=='f':		#If command is f, move forward by dist/4 encoder counts
+			dist=int(cmd[2:])/divider
+			enc_tgt(1,1,dist)
+			fwd()
+			if en_turtle:
+				turtle.forward(dist*divider)
+				
+		elif cmd[0]=='l':	#If command is l, rotate left
+			angle=int(cmd[2:])
+			if angle >360 or angle <0:
+				print "Wrong angle"
+				continue
+				
+			c.update()
+			start=360-c.headingDegrees	# compass counts go from 360 -> 0 when turning left, so invert the count
+			target= (start+angle)%360	# If target >360 degrees, wrap it to 0
+			left_rot()
+			while True:
+				current=360-c.headingDegrees
+				if debug:
+					print start,target,current
+				if target-start>0:		# Stop when target reached (works when start and target <360
+					if current>target:
+						right_rot()
+						time.sleep(.15)
+						stop()
+						break;
+				else:
+					if current>target and current <start-5:	#If target has been wrapped then the check condition changes and keep some tolerence 
+						right_rot()
+						time.sleep(.15)
+						stop()
+						break;
+				c.update()
+				#time.sleep(.1)
+			if en_turtle:
+				turtle.left(angle)			
+				
+		elif cmd [0]=='r': 				#Rotate right if command if r
+			angle=int(cmd[2:]) 
+			if angle >360 or angle <0:
+				print "Wrong angle"
+				continue
+				
+			c.update()
+			start=c.headingDegrees
+			target= (start+angle)%360
+			right_rot()
+			while True:
+				current=c.headingDegrees
+				if debug:
+					print start,target,current
+				if target-start>0:
+					if current>target:
+						stop()
+						break;
+				else:
+					if current>target and current <start-5:
+						stop()
+						break;
+				c.update()
+				#time.sleep(.1)
+			if en_turtle:
+				turtle.right(angle)
+				
+		elif cmd[0]=='x':	#Exit on x
+			print "Exiting"
+			if en_turtle:
+				turtle.bye()
+			break
+			
+		elif cmd[0]=='d':	#Show the current reading from the compass
+			c.update()
+			print c.headingDegrees,360-c.headingDegrees
+		else:
+			print "Wrong command"
+	except ValueError:
+		print "Wrong command"
+<<<<<<< HEAD
+	
+	time.sleep(.1)
+=======
+	#print heading
+	
+	time.sleep(.1)
+	
+>>>>>>> parent of 1924e7e... File name changes.
