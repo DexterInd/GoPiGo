4653226e0e3b918fea8ed64cc14ba7316a8f7a27
Dexter Industries <dxtrindustries@gmail.com>
2014-09-26 10:50:32 -0400

    Update firmware update script
    Updated firmware_update.sh:
    - Add a disconnected motors check.
    - Updated fuses.
    - Added a sudo check.

--- Firmware/firmware_update.sh
+++ Firmware/firmware_update.sh
@@ -1,5 +1,36 @@
 #! /bin/bash
-echo "Updating the GoPiGo firmware"
-echo "============================="
-avrdude -c gpio -p m328p -U flash:w:fw_ver_10.cpp.hex
-gpg
+
+if [ "$(whoami)" != 'root' ]; then
+        echo "You have no permission to run $0 as non-root user."
+        echo "Run using sudo and try again."
+        exit 1;
+fi
+
+echo "ATTENTION! Important!"
+echo "BEFORE PROGRAMMING THE GOPIGO FIRMWARE, DISCONNECT THE MOTORS."
+echo "Please confirm that you've disconnected the motors."
+echo "Have you disconnected the motors before programming the firmware? (y/n)"
+
+read motors
+y='y'
+
+if [ $motors = $y ]; then
+   echo "Updating the GoPiGo firmware"
+   echo "============================="
+
+   data='date'
+   now=$($data)
+   echo "$now"
+
+   var='avrdude -c gpio -p m328p -U lfuse:w:0x7F:m'
+   output=$($var)
+   echo "$output"
+
+   avrdude -c gpio -p m328p -U flash:w:fw_ver_10.cpp.hex
+   echo "============================="
+   echo "Check for GoPiGo Present:"
+   gpg
+   echo "============================="
+else
+  echo "Disconect your motors and retry!"
+fi
