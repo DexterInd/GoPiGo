7a898ec4b086d7cfee8fb2435eccf087cbceb34c
Biancaniello, Michael <chepazzo@gmail.com>
2015-05-15 22:46:10 -0400

    Added function to determine number of encoder pulses per cm of travel.

--- Software/Scratch/GoPiGoScratch.py
+++ Software/Scratch/GoPiGoScratch.py
@@ -24,6 +24,23 @@ en_debug=1
 #or 5 deg/pulse
 #Deg:Pulse Ratio
 DPR = 360.0/64
+WHEEL_RAD = 3.25 # Wheels are ~6.5 cm diameter. 
+CHASS_WID = 13.5 # Chassis is ~13.5 cm wide.
+
+## This should probably be moved into a gopigo python module.
+def cm2pulse(dist):
+    '''
+    Calculate the number of pulses to move the chassis dist cm.
+    pulses = dist * [pulses/revolution]/[dist/revolution]
+    '''
+    wheel_circ = 2*math.pi*WHEEL_RAD # [cm/rev] cm traveled per revolution of wheel
+    print 'WHEEL_RAD',WHEEL_RAD
+    revs = dist/wheel_circ
+    print 'revs',revs
+    PPR = 18 # [p/rev] encoder Pulses Per wheel Revolution
+    pulses = PPR*revs # [p] encoder pulses required to move dist cm.
+    print 'pulses',pulses
+    return pulses
 
 try:
     s = scratch.Scratch()
@@ -73,13 +90,21 @@ while True:
 				stop()
 			if en_debug:
 				print msg
-		elif msg=="FORWARD":
+		elif msg[:7]=="FORWARD":
 			if en_gpg:
+				if len(msg) > 7:
+					dist = int(msg[7:])
+					pulse = int(cm2pulse(dist))
+					enc_tgt(1,1,pulse)
 				fwd()
 			if en_debug:
 				print msg
-		elif msg=="BACKWARD":
+		elif msg[:8]=="BACKWARD":
 			if en_gpg:
+				if len(msg) > 8:
+					dist = int(msg[8:])
+					pulse = int(cm2pulse(dist))
+					enc_tgt(1,1,pulse)
 				bwd()
 			if en_debug:
 				print msg
@@ -117,6 +142,7 @@ while True:
 				print msg
 			r_led_pow=int(msg[4:])
 			if en_gpg:
+				## Why is the 'if' in here twice?
 				if en_gpg:
 					if r_led_pow > 127:
 						led_on(0)
@@ -153,20 +179,18 @@ while True:
 				print msg
 			srv_pos=int(msg[3:])
 			if en_gpg:
-				# Why is this done 2x??
-				if en_gpg:
-					if srv_pos > 180:
-						srv_pos = 180
-					elif srv_pos < 0:
-						srv_pos = 0
-					servo(srv_pos)
+				if srv_pos > 180:
+					srv_pos = 180
+				elif srv_pos < 0:
+					srv_pos = 0
+				servo(srv_pos)
 		elif msg=="GET_DIST":
 			if en_debug:
 				print "Received distance request."
 				print msg
 			dist= us_dist(15)
-				if en_gpg:
-					s.sensorupdate({'distance':dist})
+			if en_gpg:
+				s.sensorupdate({'distance':dist})
 		else:
 			if en_debug:
 				print "m",msg
