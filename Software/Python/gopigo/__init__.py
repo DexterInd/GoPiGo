1dd5f6029d1dbfa6a7bb111d55ec9f76cae3a3db
Biancaniello, Michael <chepazzo@gmail.com>
2015-06-18 00:38:17 -0400

    Converted the python module into a package.

---
+++ Software/Python/gopigo/__init__.py
@@ -0,0 +1,13 @@
+'''
+Drivers and examples for using the GoPiGo in Python
+'''
+
+__version__ = '0.9'
+__author__ = 'Dexter Industries'
+__maintainer__ = 'Dexter Industries'
+#__email__ = 'karan25991@gmail.com'
+__url__ = 'http://www.dexterindustries.com/GoPiGo/'
+__shortdesc__ = 'This library is used for communicating with the GoPiGo.'
+
+from .base import *
+
