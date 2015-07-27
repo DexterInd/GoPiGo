1dffeb60810aa247d2e1eb0e1148dfd157d61a1c
Dexter Industries <dxtrindustries@gmail.com>
2015-07-27 09:30:32 +0400

    License Finish
    Finished adding GPL3 to everything.

--- Software/Python/Examples/Browser Streaming Robot/robot_controller.py
+++ Software/Python/Examples/Browser Streaming Robot/robot_controller.py
@@ -7,8 +7,24 @@
 # Author     Date      		Comments
 # Karan      24 July 14  	Initial Authoring         
 # Karan		 19 Feb 15		Converted to 1 joystick mode                                                 
-# These files have been made available online through a Creative Commons Attribution-ShareAlike 3.0  license.
-# (http://creativecommons.org/licenses/by-sa/3.0/)           
+'''
+## License
+ GoPiGo for the Raspberry Pi: an open source robotics platform for the Raspberry Pi.
+ Copyright (C) 2015  Dexter Industries
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/gpl-3.0.txt>.
+'''      
 #
 # This example is derived from the Dawn Robotics Raspberry Pi Camera Bot
 # https://bitbucket.org/DawnRobotics/raspberry_pi_camera_bot
