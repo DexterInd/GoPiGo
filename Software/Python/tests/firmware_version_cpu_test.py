ad53a5b9aff230667288f4e47f6ccd275155ff83
Karan Nayan <karan25991@gmail.com>
2015-10-02 09:41:36 +0530

    GoPiGo troubleshooting script added

---
+++ Software/Python/tests/firmware_version_cpu_test.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python
+from gopigo import *
+print "v",fw_ver()
+print "CPU speed:",cpu_speed()
+print volt(),"V"
---
+++ Troubleshooting/motor_enc_led_test.sh
@@ -0,0 +1,3 @@
+#! /bin/bash
+echo ""
+sudo python /home/pi/Desktop/GoPiGo/Software/Python/tests/enc_tgt_test.py
