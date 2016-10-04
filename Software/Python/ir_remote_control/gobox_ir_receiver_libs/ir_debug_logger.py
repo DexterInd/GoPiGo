141ac78483ea8be5844f657b366387f00b34d576
Karan Nayan <karan25991@gmail.com>
2016-10-04 21:13:57 +0530

    Python library for the updated IR receiver added

---
+++ Software/Python/ir_remote_control/gobox_ir_receiver_libs/ir_debug_logger.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python 
+from subprocess import Popen, PIPE, STDOUT, call
+import datetime
+import time
+
+call("sudo /etc/init.d/lirc stop", shell=True)
+time.sleep(.5)
+
+p = Popen('mode2 -d /dev/lirc0', stdout = PIPE, stderr = STDOUT, shell = True)
+print "Logging started, Press Ctrl+C to stop"
+f=open("/home/pi/Desktop/ir_raw_sig_log.txt","a")
+f.write(str(datetime.datetime.now()))
+f.close()
+    
+while True:
+    # Read the raw value from the IR receiver
+    
+    line = p.stdout.readline()
+    if len(line)!=0:
+        f=open("/home/pi/Desktop/ir_raw_sig_log.txt","a")
+        f.write(line)
+        f.close()
+        print line,
