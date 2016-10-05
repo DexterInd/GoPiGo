c834759bef8828067a1cc82263e8bee80540211e
Karan Nayan <karan25991@gmail.com>
2016-10-05 19:30:08 +0530

    Filepaths updated

--- Software/Python/ir_remote_control/gobox_ir_receiver_libs/install.sh
+++ Software/Python/ir_remote_control/gobox_ir_receiver_libs/install.sh
@@ -2,14 +2,14 @@
 
 #Install the ir_receiver library systemwide
 sudo python /home/pi/Desktop/GoPiGo/Software/Python/ir_remote_control/gobox_ir_receiver_libs/setup.py install
-sudo rm -r /home/pi/Desktop/GoPiGo/Software/Python/ir_remote_control/gobox_ir_receiver_libs/build
-sudo rm -r /home/pi/Desktop/GoPiGo/Software/Python/ir_remote_control/gobox_ir_receiver_libs/dist
-sudo rm -r /home/pi/Desktop/GoPiGo/Software/Python/ir_remote_control/gobox_ir_receiver_libs/ir_receiver.egg-info		
+sudo rm -r build
+sudo rm -r dist
+sudo rm -r ir_receiver.egg-info		
 
-sudo chmod +x di_ir_reader_wrapper_monit.sh
+sudo chmod +x /home/pi/Desktop/GoPiGo/Software/Python/ir_remote_control/gobox_ir_receiver_libs/di_ir_reader_wrapper_monit.sh
 
 #https://www.digitalocean.com/community/tutorials/how-to-install-and-configure-monit
-sudo apt-get install monit
+sudo apt-get install monit -y
 
-sudo cp monitrc /etc/monit/monitrc
-sudo monit start all
+sudo cp /home/pi/Desktop/GoPiGo/Software/Python/ir_remote_control/gobox_ir_receiver_libs/monitrc /etc/monit/monitrc
+sudo monit start all 
