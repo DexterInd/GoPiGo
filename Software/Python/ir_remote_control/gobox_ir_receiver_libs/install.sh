62a3dae203a7f31d4920de8126d667c3fb342db7
cleoqc <cleoqc1124@gmail.com>
2017-01-05 21:53:46 +0000

    use variables instead of hard coded paths. change paths to point to ~/Dexter instead of ~/Desktop

--- Software/Python/ir_remote_control/gobox_ir_receiver_libs/install.sh
+++ Software/Python/ir_remote_control/gobox_ir_receiver_libs/install.sh
@@ -1,7 +1,17 @@
 #!/usr/bin/env bash
+PIHOME=/home/pi
+
+DIUPDATE=di_update
+RASPBIAN=Raspbian_For_Robots
+RASPBIAN_PATH=$PIHOME/$DIUPDATE/$RASPBIAN
+
+DEXTER=Dexter
+DEXTER_PATH=$PIHOME/$DEXTER
+
+GOPIGO_PATH=$DEXTER_PATH/GoPiGo
 
 #Install the ir_receiver library systemwide
-pushd /home/pi/Desktop/GoPiGo/Software/Python/ir_remote_control/gobox_ir_receiver_libs/
+pushd $GOPIGO_PATH/Software/Python/ir_remote_control/gobox_ir_receiver_libs/
 sudo python setup.py install
 sudo rm -r build
 sudo rm -r dist
@@ -13,5 +23,5 @@ popd
 #https://www.digitalocean.com/community/tutorials/how-to-install-and-configure-monit
 sudo apt-get install monit -y
 
-sudo cp /home/pi/Desktop/GoPiGo/Software/Python/ir_remote_control/gobox_ir_receiver_libs/monitrc /etc/monit/monitrc
+sudo cp  $GOPIGO_PATH/Software/Python/ir_remote_control/gobox_ir_receiver_libs/monitrc /etc/monit/monitrc
 sudo monit start all 
