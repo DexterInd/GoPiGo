b2935671cc3cee5908bf54c636bfc6ae06cc8330
Shoban94 <shobannarayan@gmail.com>
2017-04-07 14:41:27 +0530

    Update to document errors

--- Firmware/gopigo_firmware_update.sh
+++ Firmware/gopigo_firmware_update.sh
@@ -1,9 +1,17 @@
 #!/usr/bin/env bash
 REPO_PATH=$(readlink -f $(dirname $0) | grep -E -o "^(.*?\\GoPiGo)")
 update_gopigo_firmware(){ 
+	# Check if this function is called from a Dexter script like
+	# /Raspbian_For_Robots/upd_script/update_GoPiGo_Firmware.sh
 	if [[ "$1" == "Dexter" ]]
 	then
 	    REPO_PATH=/home/pi/Dexter/GoPiGo
+	fi
+	# Checking if the REPO_PATH contains GoPiGo folder
+	if ! echo "$REPO_PATH" | grep -q "/GoPiGo"
+	then
+	    echo "Error in REPO_PATH:$REPO_PATH"
+	    exit
 	fi
 	echo "$REPO_PATH"
 	sudo avrdude -c gpio -p m328p -U lfuse:w:0x7F:m
