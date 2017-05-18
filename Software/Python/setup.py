e5e5047380c4b7ba91ed2c6e45302311ed12d22c
Dexter Industries <dxtrindustries@gmail.com>
2017-05-18 09:59:49 +0400

    Install Mutex on Install
    Mutex didn't install.
    Now it should.  Thus rendering easygopigo usable.  :-)

--- Software/Python/setup.py
+++ Software/Python/setup.py
@@ -21,10 +21,10 @@ along with this program.  If not, see <http://www.gnu.org/licenses/gpl-3.0.txt>.
 
 import setuptools
 setuptools.setup(
-	name="GoPiGo",
-	description="Drivers and examples for using the GoPiGo in Python",
-	author="Dexter Industries",
-	url="http://www.dexterindustries.com/GoPiGo/",
-	py_modules=['gopigo','easygopigo','line_follower.line_sensor'],
-	install_requires=open('requirements.txt').readlines(),
+    name="GoPiGo",
+    description="Drivers and examples for using the GoPiGo in Python",
+    author="Dexter Industries",
+    url="http://www.dexterindustries.com/GoPiGo/",
+    py_modules=['I2C_mutex','gopigo','easygopigo','line_follower.line_sensor'],
+    install_requires=open('requirements.txt').readlines(),
 )
