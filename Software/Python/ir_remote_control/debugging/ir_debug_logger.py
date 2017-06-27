f203c43683c1edc4b1f92492ed5168564a9cb44f
Dexter Industries <dxtrindustries@gmail.com>
2017-06-27 18:06:57 +0400

    Remove Monit in Ir Debug

--- Software/Python/ir_remote_control/debugging/ir_debug_logger.py
+++ Software/Python/ir_remote_control/debugging/ir_debug_logger.py
@@ -7,7 +7,7 @@ call("sudo /etc/init.d/lirc stop", shell=True)
 time.sleep(.5)
 
 # Stop the DI ir reader running in the background to enable the debug logging 
-p = Popen('sudo monit stop di_ir_reader', stdout = PIPE, stderr = STDOUT, shell = True) 
+# p = Popen('sudo monit stop di_ir_reader', stdout = PIPE, stderr = STDOUT, shell = True) 
 
 p = Popen('mode2 -d /dev/lirc0', stdout = PIPE, stderr = STDOUT, shell = True)
 print "Logging started, Press Ctrl+C to stop"
