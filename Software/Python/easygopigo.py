a6779053fec6de908ca5830f2eb8f6453ca11ec5
Nicole Parrot <cleoqc1124@gmail.com>
2018-02-23 14:40:23 -0500

    Feature/systemwide mutex (#290)
    * read_position_str() is also needed here to keep in sync with easygopigo3
    
    * support systemwide mutex

--- Software/Python/easygopigo.py
+++ Software/Python/easygopigo.py
@@ -19,19 +19,22 @@ except:
 from I2C_mutex import Mutex
 
 mutex = Mutex()
+overall_mutex = mutex.overall_mutex()
 
 def _ifMutexAcquire(mutex_enabled = False):
     """
     Acquires the I2C if the ``use_mutex`` parameter of the constructor was set to ``True``.
+    Always acquires if system-wide mutex has been set.
+    
     """
-    if mutex_enabled:
+    if mutex_enabled or overall_mutex==True:
         mutex.acquire()
 
 def _ifMutexRelease(mutex_enabled = False):
     """
     Releases the I2C if the ``use_mutex`` parameter of the constructor was set to ``True``.
     """
-    if mutex_enabled:
+    if mutex_enabled or overall_mutex==True:
         mutex.release()
 
 try:
