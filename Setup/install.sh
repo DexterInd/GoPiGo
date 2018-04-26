c472a4b661a8a5189f7104f5b4813766a6498da2
Robert Lucian CHIRIAC <robert.lucian.chiriac@gmail.com>
2018-04-26 17:15:08 +0300

    feature - take the line follower install functions out

--- Setup/install.sh
+++ Setup/install.sh
@@ -197,45 +197,6 @@ install_avr() {
     echo "done with AVRDUDE "
 }
 
-install_line_follower(){
-    feedback "--> Installing Line Follower Calibration"
-    # Install GoPiGo Line Follower Calibration
-    delete_file /home/pi/Desktop/line_follow.desktop
-    sudo cp /home/pi/Dexter/GoPiGo/Software/Python/line_follower/line_follow.desktop /home/pi/Desktop/
-    sudo chmod +x /home/pi/Desktop/line_follow.desktop
-    sudo chmod +x /home/pi/Dexter/GoPiGo/Software/Python/line_follower/line_sensor_gui.py
-
-    # if the configuration files exist in the home directory
-    # then move them to their new place
-    # otherwise create new ones
-    if file_exists "$PIHOME/black_line.txt"
-    then
-        sudo mv $PIHOME/black_line.txt $PIHOME/Dexter/black_line.txt
-    else
-        sudo cp $PIHOME/Dexter/GoPiGo/Software/Python/line_follower/black_line.txt $PIHOME/Dexter/black_line.txt
-    fi
-
-    if file_exists "$PIHOME/white_line.txt"
-    then
-        sudo mv $PIHOME/white_line.txt $PIHOME/Dexter/white_line.txt
-    else
-        sudo cp $PIHOME/Dexter/GoPiGo/Software/Python/line_follower/white_line.txt $PIHOME/Dexter/white_line.txt
-    fi
-    if file_exists "$PIHOME/range_line.txt"
-    then
-        sudo mv $PIHOME/range_line.txt $PIHOME/Dexter/range_line.txt
-    else
-        sudo cp $PIHOME/Dexter/GoPiGo/Software/Python/line_follower/range_line.txt $PIHOME/Dexter/range_line.txt
-    fi
-
-    sudo chmod 666 $PIHOME/Dexter/*line.txt
-
-}
-
-install_control_panel(){
-    sudo cp "$ROBOT_DIR/Software/Python/control_panel/gopigo_control_panel.desktop" $PIHOME/Desktop
-}
-
 call_for_reboot() {
     if ! quiet_mode ; then
         feedback " "
@@ -283,8 +244,6 @@ install_wiringpi
 install_spi_i2c
 # no longer installing avr for arduino
 # install_avr
-install_line_follower
-install_control_panel
 
 #sudo rm -r /tmp/di_update
 
