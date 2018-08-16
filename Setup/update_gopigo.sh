af98c60450fabe5e1b2b3d81d41c26e4a4b5ff77
Robert Lucian CHIRIAC <robert.lucian.chiriac@gmail.com>
2018-08-16 22:08:15 +0300

    hotfix - remove script_tools & bypass gui installation

--- Setup/update_gopigo.sh
+++ Setup/update_gopigo.sh
@@ -35,6 +35,7 @@ parse_cmdline_arguments() {
   updaterepo=true
   install_rfrtools=true
   install_pkg_rfrtools=true
+  install_rfrtools_gui=true
 
   # the following 3 options are mutually exclusive
   systemwide=true
@@ -60,6 +61,9 @@ parse_cmdline_arguments() {
         ;;
       --bypass-python-rfrtools)
         install_pkg_rfrtools=false
+        ;;
+      --bypass-gui-installation)
+        install_rfrtools_gui=false
         ;;
       --user-local)
         userlocal=true
@@ -113,18 +117,10 @@ parse_cmdline_arguments() {
   ([[ $updaterepo = "true" ]] && echo "  --no-update-aptget=false") || echo "  --no-update-aptget=true"
   ([[ $install_rfrtools = "true" ]] && echo "  --bypass-rfrtools=false") || echo "  --bypass-rfrtools=true"
   ([[ $install_pkg_rfrtools = "true" ]] && echo "  --bypass-python-rfrtools=false") || echo "  --bypass-python-rfrtools=true"
+  ([[ $install_rfrtools_gui = "true" ]] && echo "  --bypass-gui-installation=false") || echo "  --bypass-gui-installation=true"
   echo "  --user-local=$userlocal"
   echo "  --env-local=$envlocal"
   echo "  --system-wide=$systemwide"
-
-  # in case the following packages are not installed and `--no-dependencies` option has been used
-  if [[ $installdependencies = "false" || $install_rfrtools = "false" ]]; then
-    command -v git >/dev/null 2>&1 || { echo "This script requires \"git\" but it's not installed. Don't use --no-dependencies option. Exiting." >&2; exit 1; }
-    command -v python >/dev/null 2>&1 || { echo "Executable \"python\" couldn't be found. Don't use --no-dependencies option. Exiting." >&2; exit 2; }
-    command -v python3 >/dev/null 2>&1 || { echo "Executable \"python3\" couldn't be found. Don't use --no-dependencies option. Exiting." >&2; exit 3; }
-    command -v pip >/dev/null 2>&1 || { echo "Executable \"pip\" couldn't be found. Don't use --no-dependencies option. Exiting." >&2; exit 4; }
-    command -v pip3 >/dev/null 2>&1 || { echo "Executable \"pip3\" couldn't be found. Don't use --no-dependencies option. Exiting." >&2; exit 5; }
-  fi
 
   # create rest of list of arguments for rfrtools call
   rfrtools_options+=("$selectedbranch")
@@ -133,20 +129,32 @@ parse_cmdline_arguments() {
   [[ $installdependencies = "true" ]] && rfrtools_options+=("--install-deb-deps")
   [[ $install_pkg_rfrtools = "true" ]] && rfrtools_options+=("--install-python-package")
 
-  # create list of arguments for script_tools call
-  declare -ga scriptools_options=("$selectedbranch")
-
   echo "Using \"$selectedbranch\" branch"
   echo "Options used for RFR_Tools script: \"${rfrtools_options[@]}\""
-  echo "Options used for script_tools script: \"${scriptools_options[@]}\""
 }
 
 #################################################
-### Cloning GoPiGo, Script_Tools & RFR_Tools ####
+######### Cloning GoPiGo & RFR_Tools ############
 #################################################
 
+# called in <<install_rfrtools_repo>>
+check_dependencies() {
+  command -v git >/dev/null 2>&1 || { echo "This script requires \"git\" but it's not installed. Error occurred with RFR_Tools installation." >&2; exit 1; }
+  command -v python >/dev/null 2>&1 || { echo "Executable \"python\" couldn't be found. Error occurred with RFR_Tools installation." >&2; exit 2; }
+  command -v pip >/dev/null 2>&1 || { echo "Executable \"pip\" couldn't be found. Error occurred with RFR_Tools installation." >&2; exit 3; }
+  if [[ $usepython3exec = "true" ]]; then
+    command -v python3 >/dev/null 2>&1 || { echo "Executable \"python3\" couldn't be found. Error occurred with RFR_Tools installation." >&2; exit 4; }
+    command -v pip3 >/dev/null 2>&1 || { echo "Executable \"pip3\" couldn't be found. Error occurred with RFR_Tools installation." >&2; exit 5; }
+  fi
+
+  if [[ ! -f $DEXTERSCRIPT/functions_library.sh ]]; then
+    echo "script_tools didn\'t get installed. Enable the installation of dependencies with RFR_Tools.'"
+    exit 7
+  fi
+}
+
 # called way down below
-install_scriptools_and_rfrtools() {
+install_rfrtools_repo() {
 
   # if rfrtools is not bypassed then install it
   if [[ $install_rfrtools = "true" ]]; then
@@ -161,20 +169,6 @@ install_scriptools_and_rfrtools() {
     fi
     echo "Done installing RFR_Tools"
   fi
-
-  # update script_tools first
-  curl --silent -kL https://raw.githubusercontent.com/DexterInd/script_tools/$selectedbranch/install_script_tools.sh > $PIHOME/.tmp_script_tools.sh
-  echo "Installing script_tools. This might take a while.."
-  bash $PIHOME/.tmp_script_tools.sh $selectedbranch > /dev/null
-  ret_val=$?
-  rm $PIHOME/.tmp_script_tools.sh
-  if [[ $ret_val -ne 0 ]]; then
-    echo "script_tools failed installing with exit code $ret_val. Exiting."
-    exit 6
-  fi
-  # needs to be sourced from here when we call this as a standalone
-  source $DEXTERSCRIPT/functions_library.sh
-  feedback "Done installing script_tools"
 }
 
 # called way down bellow
@@ -268,8 +262,11 @@ install_python_pkgs_and_dependencies() {
 ################################################
 
 check_if_run_with_pi
+
 parse_cmdline_arguments "$@"
-install_scriptools_and_rfrtools
+install_rfrtools_repo
+
 clone_gopigo
 install_python_pkgs_and_dependencies
+
 exit 0
