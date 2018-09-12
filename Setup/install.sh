9240a7bde751783ab847899da58669008a901948
Robert Lucian Chiriac <robert.lucian.chiriac@gmail.com>
2018-09-12 22:00:55 +0300

    hotfix - add gui option & optimize install script (#316)
    * hotfix - remove script_tools & bypass gui installation
    
    * hotfix - change github references for testing purposes
    
    * hotfix - modify README accordingly & change how deb pkgs are installed
    
    * hotfix - add missing function call
    
    * hotfix - change branch for github clone for testing purposes
    
    * hotfix - assign the correct exit codes
    
    * hotfix - remove unnecessary option for minimal ist
    
    * hotfix - add missing option
    
    * Revert "hotfix - change github references for testing purposes"
    
    This reverts commit 306cd03ef8439dfdb73e7956f95266b39e34c54f.

--- Setup/install.sh
+++ Setup/install.sh
@@ -17,9 +17,10 @@ install_dependencies() {
     # update_gopigo.sh
 
     feedback "Installing dependencies for the GoPiGo"
-    sudo apt-get install git libi2c-dev i2c-tools minicom libnss-mdns build-essential libffi-dev -y
-    sudo apt-get install python-pip python-serial python-rpi.gpio python-smbus python-dev python-numpy -y
-    sudo apt-get install python3-pip python3-serial python3-rpi.gpio python3-smbus python3-dev python3-numpy -y
+    sudo apt-get install --no-install-recommends -y \
+        git libi2c-dev i2c-tools libnss-mdns build-essential libffi-dev \
+        python-pip python-serial python-rpi.gpio python-smbus python-dev \
+        python3-pip python3-serial python3-rpi.gpio python3-smbus python3-dev
 
     feedback "Dependencies installed for the GoPiGo"
 }
